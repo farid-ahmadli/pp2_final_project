784aab1c31526bcac3205c2a1af8b6f6c02a68c5
Farid Ahmadov <faridar@gmail.com>
2021-12-09 16:37:59 +0400

    Minor change

--- Team_15/Main.java
+++ Team_15/Main.java
@@ -2,5 +2,7 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println("Hello, this is team 15!");
+
+        System.out.println("Version 2, minor updates");
     }
 }
